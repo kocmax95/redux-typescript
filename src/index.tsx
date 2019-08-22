7f57313ed006d6f438e5073cce4275748f39bcf9
kocmax95 <maksymiliankoczaski@gmail.com>
2019-08-22 16:21:51 +0200

    start

---
+++ src/index.tsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App} from './components/App'
+import { reducers } from './reducers/index'
+
+const store = createStore(reducers, applyMiddleware(thunk))
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+);
+
+
+
+//interface AppProps {
+    //     color: string;
+    // }
+
+    // interface AppState {
+    //     counter: number;
+    // }
+
+    // //Function Component
+    // // const App = (props: AppProps): JSX.Element => {
+    // //     return <div>{props.color}</div>
+    // // }
+
+    // class App extends React.Component<AppProps, AppState> {
+
+    //     constructor(props: AppProps){
+    //         super(props)
+
+    //         this.state={counter: 0}
+    //     }
+    //     onIncrement=(): void =>{
+    //         this.setState({
+    //             counter: this.state.counter + 1
+    //         })
+    //     }
+    //     onDecrement=(): void=>{
+    //         this.setState({
+    //             counter: this.state.counter -1
+    //         })
+    //     }
+    //     render(){
+    //         return( 
+    //             <div>
+    //                 <button onClick={this.onIncrement}>Increment</button>
+    //                 <button onClick={this.onDecrement}>Decrement</button>
+    //                 {this.state.counter}
+    //             </div>
+    //         )
+    //     }
+    // }
